efdd62a27578b5dddaf0212b70ea0a92fa79868e
JohnGastone <johngastone11@gmail.com>
2025-12-11 10:31:53 +0300

    Update application title to 'Mwanafunzi Portal'

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Mwanafunzi Portal',
       debugShowCheckedModeBanner: false, // Add this line
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
