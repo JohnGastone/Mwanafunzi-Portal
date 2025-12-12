366238e030b72723405810f8831aa6b3c56b1d95
JohnGastone <johngastone11@gmail.com>
2025-12-12 09:34:23 +0300

    Update build.gradle.kts to use Flutter versioning for compile and target SDKs

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 
 android {
     namespace = "com.example.mwanafunziportal"
-    compileSdk = 35
+    compileSdk = flutter.compileSdkVersion
     ndkVersion = "29.0.14206865"
 
     compileOptions {
@@ -24,10 +24,10 @@ android {
         applicationId = "com.example.mwanafunziportal"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = 24
-        targetSdk = 35
+        minSdk = flutter.minSdkVersion
+        targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
-        versionName = 1.1.0
+        versionName = flutter.versionName
     }
 
     buildTypes {
